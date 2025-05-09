6a3e4b79ea9692095977a27fbf294df13942dd8e
ritesh <riteshsaini1601@gmail.com>
2025-05-09 22:56:10 +0530

    Setup
    Setup completed with custom globals.css file and updating layout , added custom font files that is being used

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className="underline text-2xl font-bold text-center font-karla">
+        Welcome
+      </h1>
+    </div>
+  )
+}
+
+export default Page
